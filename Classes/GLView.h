8cc7b9ffd94f8e7faaa5837717a689045840b83f
fangxing <fangxinga@chanjet.com>
2017-07-30 14:16:18 +0800

    《OpenGL ES 从零开始系列》Template

---
+++ Classes/GLView.h
@@ -0,0 +1,42 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+
+@protocol GLViewDelegate
+- (void)drawView:(UIView *)theView;
+- (void)setupView:(UIView *)theView;
+@end
+
+@interface GLView : UIView 
+{
+    
+@private
+
+    GLint backingWidth;
+    GLint backingHeight;
+    
+    EAGLContext *context;    
+    GLuint viewRenderbuffer, viewFramebuffer;
+    GLuint depthRenderbuffer;
+    
+    NSTimer *animationTimer;
+    NSTimeInterval animationInterval;
+    
+    id <GLViewDelegate>     delegate;
+}
+@property (nonatomic, assign) NSTimeInterval animationInterval;
+@property (nonatomic, assign) /* weak ref */ id <GLViewDelegate> delegate;
+- (void)startAnimation;
+- (void)stopAnimation;
+- (void)drawView;
+@end
---
+++ Classes/OpenGLTexture3D.h
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+
+@interface OpenGLTexture3D : NSObject {
+	GLuint		texture[1];  
+	NSString	*filename;
+}
+@property (nonatomic, retain) NSString *filename;
+- (id)initWithFilename:(NSString *)inFilename width:(GLuint)inWidth height:(GLuint)inHeight;
+- (void)bind;
++ (void)useDefaultTexture;
+@end
